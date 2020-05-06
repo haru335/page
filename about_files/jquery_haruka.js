3aff21b4e5d96992e0d4a5684149adb8d086e37c
Haruka Ikeda <f25729888@yahoo.co.jp>
2020-05-06 19:28:44 +0900

    Menu 切替　　CSS　追加

--- about_files/jquery_haruka.js
+++ about_files/jquery_haruka.js
@@ -1,33 +1,5 @@
-
-
-$(function() {
-  /*
-  var $win = $(window),
-      $main = $('#intro'),
-      $nav = $('nav'),
-      navHeight = $nav.outerHeight(),
-      navPos = $nav.offset().top,
-      fixedClass = 'is-fixed';
-
-  $win.on('load scroll', function() {
-    var value = $(this).scrollTop();
-    if ( value > navPos ) {
-      $nav.addClass(fixedClass);
-      $main.css('margin-top', navHeight);
-    } else {
-      $nav.removeClass(fixedClass);
-      $main.css('margin-top', '0');
-    }
-  });*/
-});
-
-
-$(function() {
-  //$('h3').fadeOut(2000);
-});
-
-
 // JavaScript Document
+
 window.onload = function() {
 
 	// ABOUT ME MODAL
