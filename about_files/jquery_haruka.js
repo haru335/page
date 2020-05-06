2cc778f9f1796160b10542293d06f9529a796e8d
Haruka Ikeda <f25729888@yahoo.co.jp>
2020-05-06 15:35:47 +0900

    menu change  (tmp)

---
+++ about_files/jquery_haruka.js
@@ -0,0 +1,26 @@
+
+
+$(function() {
+  var $win = $(window),
+      $main = $('#intro'),
+      $nav = $('nav'),
+      navHeight = $nav.outerHeight(),
+      navPos = $nav.offset().top,
+      fixedClass = 'is-fixed';
+
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > navPos ) {
+      $nav.addClass(fixedClass);
+      $main.css('margin-top', navHeight);
+    } else {
+      $nav.removeClass(fixedClass);
+      $main.css('margin-top', '0');
+    }
+  });
+});
+
+
+$(function() {
+  //$('h3').fadeOut(2000);
+});
